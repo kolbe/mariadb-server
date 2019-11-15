midenok/mariadb

MDEV-16978 Application-time periods: WITHOUT OVERLAPS
* The overlaps check is implemented on a handler level per row command. It creates a separate cursor (actually, another handler instance) and caches it inside the original handler, when ha_update_row or ha_insert_row is issued. Cursor closes on unlocking the handler.

* Containing the same key in index means unique constraint violation even in usual terms. So we fetch left and right neighbours and check that they have same key prefix, excluding from the key only the period part. If it doesnt match, then there's no such neighbour, and the check passes. Otherwise, we check if this neighbour intersects with the considered key.

* the check does introduce new error and fails with ER_DUPP_KEY error. This might break REPLACE workflow and should be fixed separately

sql/table.cc
@@ -1929,8 +1929,18 @@ int TABLE_SHARE::init_from_binary_frm_image(THD *thd, bool write,
     if (init_period_from_extra2(period, field_pos))
       goto err;
 
+    const uchar *key_pos= field_pos + 2 * frm_fieldno_size;
+    period.unique_keys= uint2korr(key_pos);
+    for (uint k= 0; k < period.unique_keys; k++)
+    {
+      key_pos+= frm_keyno_size;
+      uint key_nr= uint2korr(key_pos);
+      key_info[key_nr].without_overlaps= true;
+    }
+
     if (period.name.length + period.constr_name.length
-          + 2 * frm_ident_len_size + 2 * frm_fieldno_size
+          + (period.unique_keys + 1) * frm_keyno_size + 2 * frm_fieldno_size
+          + 2 * frm_ident_len_size
         != extra2.application_period.length)
       goto err;
   }

kolbe/mariadb-server

sql/table.cc
@@ -2253,6 +2253,21 @@ int TABLE_SHARE::init_from_binary_frm_image(THD *thd, bool write,
     if (init_period_from_extra2(&period, pos, end))
       goto err;
     status_var_increment(thd->status_var.feature_application_time_periods);
+
+    const uchar *key_pos= pos + 2 * frm_fieldno_size;
+    period.unique_keys= read_frm_keyno(key_pos);
+    for (uint k= 0; k < period.unique_keys; k++)
+    {
+      key_pos+= frm_keyno_size;
+      uint key_nr= read_frm_keyno(key_pos);
+      key_info[key_nr].without_overlaps= true;
+    }
+
+    if (frm_ident_stored_size(period.name.length)
+        + frm_ident_stored_size(period.constr_name.length)
+        + (period.unique_keys + 1) * frm_keyno_size + 2 * frm_fieldno_size
+        != extra2.application_period.length)
+      goto err;
   }
 
   if (extra2.field_data_type_info.length &&